b715c4ca9293223ad4cc2735fb7c0a0ac0870be1
Jeremy Green <mutatedcreativity@gmail.com>
2018-04-24 11:12:40 -0400

    add pdf generation

--- index.js
+++ index.js
@@ -1,43 +1,78 @@
-const { readFile } = require('fs');
-
+const { readFile, createWriteStream } = require('fs');
 const { promisify } = require('util');
 
 const GoogleSpreadsheet = require('google-spreadsheet');
-
-const readFileAsync = promisify(readFile);
+const PDFDocument = require('pdfkit');
+const client = require('scp2');
 
 const spreadsheetId = '1wtzFCEh40lqH9OEArcQ7mdKhAZ3JxP1areoxOpBS6gI';
-
 const { useServiceAccountAuth, getInfo } = new GoogleSpreadsheet(spreadsheetId);
+const doc = new PDFDocument;
 
+const readFileAsync = promisify(readFile);
 const useServiceAccountAuthAsync = promisify(useServiceAccountAuth);
-
 const getInfoAsync = promisify(getInfo);
+
+async function getRows(info, tabId = 1) {
+  const { getRows } = info.worksheets[tabId];
+  const getRowsAsync = promisify(getRows);
+  return getRowsAsync();
+}
 
 function readConfig() {
   return readFileAsync('./credentials.json', 'utf-8');
 }
 
 async function init() {
-  const creds = await readConfig();
-  const json = JSON.parse(creds);
-  await useServiceAccountAuthAsync(json);
-
   try {
-    const info = await getInfoAsync();
-    const { getRows } = info.worksheets[1];
-    const getRowsAsync = promisify(getRows);
-    const rows = await getRowsAsync();
-    const bagelData = rows.map(row => row.type.toLowerCase()).filter(item => item.trim() !== '').reduce((allNames, name) => {
-      if (name in allNames) {
-        allNames[name]++;
-      } else {
-        allNames[name] = 1;
-      }
-      return allNames;
-    }, {});
+    const creds = await readConfig();
+    const json = JSON.parse(creds);
 
-    console.log(bagelData);
+    await useServiceAccountAuthAsync(json);
+
+    const info = await getInfoAsync();
+    const bagelCount = await getRows(info, 1);
+    const contactInfo = await getRows(info, 2);
+
+    doc.pipe(createWriteStream('./bagel.pdf'));
+    doc.fontSize(24);
+    doc.text(`${info.title}`, {
+      align: 'center'
+    });
+
+    doc.fontSize(16);
+    doc.text(`\n\n`, {
+      align: 'center'
+    });
+
+    doc.fontSize(12);
+    contactInfo.map((row) => {
+      const { name, email, phone, message } = row;
+      let output = '';
+
+      output += `Name: ${name}\n`;
+      output += `Email: ${email}\n`;
+      output += `Phone Number: ${phone}\n`;
+      output += `${message}`;
+
+      doc.text(output, {
+        align: 'center'
+      });
+    });
+
+    doc.text(`\n\n`, {
+      align: 'center'
+    });
+
+    let output = '';
+    bagelCount.map((row) => {
+      output += `${row.type}: ${row.count}\n`;
+    });
+
+    doc.fontSize(16);
+    doc.text(output);
+    doc.end();
+
   } catch (error) {
     console.error(error);
   }
