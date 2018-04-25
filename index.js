8ea8315f6eaf95d5e7ebf9038968c5858808fc0d
Jeremy Green <mutatedcreativity@gmail.com>
2018-04-25 19:05:00 -0400

    add more stuff

--- index.js
+++ index.js
@@ -3,38 +3,120 @@ const { promisify } = require('util');
 
 const GoogleSpreadsheet = require('google-spreadsheet');
 const PDFDocument = require('pdfkit');
-const client = require('scp2');
+const config = require('config');
+const request = require('request-promise-native');
 
-const spreadsheetId = '1wtzFCEh40lqH9OEArcQ7mdKhAZ3JxP1areoxOpBS6gI';
-const { useServiceAccountAuth, getInfo } = new GoogleSpreadsheet(spreadsheetId);
+const { Client } = require('scp2');
+
+const googleCreds = config.get('google');
+const atlCreds = config.get('theatlantic');
+const slackInfo = config.get('slack');
+
+// stack some || statements
+if (
+  !googleCreds ||
+  !atlCreds ||
+  !slackInfo
+) {
+  console.error('There\'s some credentials missing somewhere...');
+  process.exit();
+}
+
+const { spreadsheet_id } = googleCreds;
+const { useServiceAccountAuth, getInfo } = new GoogleSpreadsheet(spreadsheet_id);
 const doc = new PDFDocument;
 
 const readFileAsync = promisify(readFile);
 const useServiceAccountAuthAsync = promisify(useServiceAccountAuth);
 const getInfoAsync = promisify(getInfo);
 
+
+/**
+ * Get the rows of the Spreadsheet
+ * @param  {Object} info  The Spreadsheet Info
+ * @param  {Number} tabId The Spreadsheet tab number
+ * @return {Promise}      A promise to return the rows
+ */
 async function getRows(info, tabId = 1) {
   const { getRows } = info.worksheets[tabId];
   const getRowsAsync = promisify(getRows);
   return getRowsAsync();
 }
 
-function readConfig() {
-  return readFileAsync('./credentials.json', 'utf-8');
+/**
+ * Handle the scp upload
+ * @param  {Buffer} content The content buffer
+ */
+function handleSCP(content) {
+  const filename = `bagel-${Date.now()}.pdf`;
+  const destination = `/www/cmsprod/shared/assets/media/files/dso-bagels/${filename}`;
+  const client = new Client(atlCreds);
+
+  client.write({ destination, content }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    sendFax(filename);
+  });
 }
 
+/**
+ * Send a fax to Twilio
+ * @param  {String} filename The file name
+ */
+function sendFax(filename) {
+  // just pass through for meow
+  handleNotifications(filename);
+}
+
+/**
+ * Notify parties of successful delivery
+ * @param  {String} filename The file name
+ */
+async function handleNotifications(filename) {
+  const options = {
+    method: 'POST',
+    uri: slackInfo.webhookURL,
+    body: {
+      channel: '@jgreen',
+      icon_emoji: ':bagel:',
+      username: 'Bagel.Bot',
+      mrkdwn: true,
+      text: `
+*I've sent the bagel order!*
+
+Hi there, this is a friendly reminder that I have sent the bagel order for the week.
+
+You can see the current order by navigating to: https://cdn.theatlantic.com/assets/media/files/dso-bagels/${filename}.
+      `
+    },
+    json: true
+  };
+
+  try {
+    const parsedBody = await request(options);
+  } catch (error) {
+    console.error(error);
+  }
+  process.exit();
+}
+
+/**
+ * Kick off the thing
+ */
 async function init() {
   try {
-    const creds = await readConfig();
-    const json = JSON.parse(creds);
-
-    await useServiceAccountAuthAsync(json);
+    await useServiceAccountAuthAsync(googleCreds);
 
     const info = await getInfoAsync();
     const bagelCount = await getRows(info, 1);
     const contactInfo = await getRows(info, 2);
 
-    doc.pipe(createWriteStream('./bagel.pdf'));
+    const buffer = [];
+
+    doc.on('data', d => buffer.push(d));
+    doc.on('end', () => handleSCP(Buffer.concat(buffer)));
+
     doc.fontSize(24);
     doc.text(`${info.title}`, {
       align: 'center'
@@ -53,7 +135,10 @@ async function init() {
       output += `Name: ${name}\n`;
       output += `Email: ${email}\n`;
       output += `Phone Number: ${phone}\n`;
-      output += `${message}`;
+
+      if (message.trim() !== '') {
+        output += `${message}`;
+      }
 
       doc.text(output, {
         align: 'center'
